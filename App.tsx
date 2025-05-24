6cc8ddf5e448b92fdcd70bdfb1d2900873c7713d
ramon541 <ramondiasmonteiro@gmail.com>
2025-05-24 18:55:44 -0300

    :package: build: Configuração EAS e adição de libs

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Colors } from './src/styles';
+// import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View
+            style={[styles.container, { backgroundColor: Colors.background }]}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+            <StatusBar style="light" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+    },
 });
---
+++ src/styles/Colors.ts
@@ -0,0 +1,16 @@
+const Colors = {
+    primary: '#0EA6E9',
+    background: '#1F1D2B',
+    shape: '#252836',
+    green: '#22B07D',
+    orange: '#FF8700',
+    red: '#FB4141',
+    black: '#171725',
+    darkGrey: '#696974',
+    grey: '#92929D',
+    whiteGrey: '#EBEBEF',
+    white: '#FFFFFF',
+    lineDark: '#EAEAEA',
+} as const;
+
+export default Colors;
